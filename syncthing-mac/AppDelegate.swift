292698da55d85fbc4ef4c5dd8398910a1507ff54
rkusa <m@rkusa.st>
2015-05-12 13:07:24 +0200

    Initial Commit

---
+++ syncthing-mac/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  syncthing-mac
+//
+//  Created by Markus Ast on 12.05.15.
+//  Copyright (c) 2015 Markus Ast. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
